534e3826f1b0e703122913f0a150281241947749
Lana Kawauchi <lanakawa@hawaii.edu>
2020-10-06 23:03:32 -1000

    final

---
+++ youtube.js
@@ -0,0 +1,3 @@
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/semantic-ui/0.12.0/javascript<ntic.min.js"></script>
+<script src="http://cdn.jsdelivr.net/jquery.glide/1.0.6/jquery.glide.min.js"></script>
